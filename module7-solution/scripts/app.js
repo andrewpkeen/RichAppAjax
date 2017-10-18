fea1b8ab98d41a52d970406188128c51472a781c
Andrew Keen <andrewpkeen@gmail.com>
2017-10-18 07:11:21 -0400

    Quantity text box and pricePerItem

--- module7-solution/scripts/app.js
+++ module7-solution/scripts/app.js
@@ -36,11 +36,11 @@ function ShoppingListCheckOffService() {
   var service = this;
 
   var itemsToBuy = [
-    { name: "bags of cookies", quantity: 10 },
-    { name: "pounds of bacon", quantity: 3 },
-    { name: "chicken", quantity: 1 },
-    { name: "eggs", quantity: 12 },
-    { name: "potatoes", quantity: 6 }
+    { name: "bags of cookies", quantity: 10, pricePerItem: 2.50 },
+    { name: "pounds of bacon", quantity: 3, pricePerItem: 5.00 },
+    { name: "chickens", quantity: 2, pricePerItem: 7.99 },
+    { name: "eggs", quantity: 12, pricePerItem: 0.30 },
+    { name: "potatoes", quantity: 6, pricePerItem: 0.60 }
   ];
 
   var boughtItems = [];
