6d86c2c674d904fbe6d11dace0fc697dc2a5fb5d
Andrew Keen <andrewpkeen@gmail.com>
2017-11-04 08:29:11 -0400

    Added comments

--- module8-solution/scripts/app.js
+++ module8-solution/scripts/app.js
@@ -28,18 +28,20 @@ function NarrowItDownController(MenuSearchService) {
 
   control.narrowItDown = function () {
     if (control.searchTerm.length === 0) {
+      // If the search term is empty, report nothing found
       control.nothingFound = true;
       control.found = [];
-      return;
+    } else {
+      // Get the search results from the service
+      var promise = MenuSearchService.getMatchedMenuItems(control.searchTerm);
+      promise.then(function (result) {
+        control.found = result;
+        control.nothingFound = control.found.length === 0;
+      });
     }
-
-    var promise = MenuSearchService.getMatchedMenuItems(control.searchTerm);
-    promise.then(function (result) {
-      control.found = result;
-      control.nothingFound = control.found.length === 0;
-    });
   };
 
+  // Function to remove menu item at given index from found array
   control.remove = function (index) {
     control.found.splice(index, 1);
   };
@@ -49,6 +51,7 @@ MenuSearchService.$inject = ['$http'];
 function MenuSearchService($http) {
   var service = this;
 
+  // Function to get the items from web service and filter on the search term
   service.getMatchedMenuItems = function(searchTerm) {
     return $http({
       method: 'GET',
