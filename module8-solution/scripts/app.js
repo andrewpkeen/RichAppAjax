7178de581667be5cad0c9f848aa17b3e0a4fbf20
Andrew Keen <andrewpkeen@gmail.com>
2017-11-04 08:02:43 -0400

    Completed initial working implementation

--- module8-solution/scripts/app.js
+++ module8-solution/scripts/app.js
@@ -2,20 +2,63 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
-.contoller('NarrowItDownController', NarrowItDownController)
+.controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
-NarrowItDownController.$inject = ['MenuSearchService']
-function NarrowItDownController(MenuSearchService) {
-  var control = this;
+function FoundItemsDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: 'founditems.html',
+    scope: {
+      items: '<',
+      showNothingFound: '<',
+      onRemove: '&'
+    }
+  };
 }
 
-function MenuSearchService() {
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var control = this;
+
+  control.searchTerm = "";
+  control.found = [];
+  control.nothingFound = false;
+
+  control.narrowItDown = function () {
+    if (control.searchTerm.length === 0) {
+      control.nothingFound = true;
+      control.found = [];
+      return;
+    }
+
+    var promise = MenuSearchService.getMatchedMenuItems(control.searchTerm);
+    promise.then(function (result) {
+      control.found = result;
+      control.nothingFound = control.found.length === 0;
+    });
+  };
+
+  control.remove = function (index) {
+    control.found.splice(index, 1);
+  };
+}
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http) {
   var service = this;
 
-  function getMatchedMenuItems(searchTerm) {
-    
-  }
+  service.getMatchedMenuItems = function(searchTerm) {
+    return $http({
+      method: 'GET',
+      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+    }).then(function (result) {
+      return result.data.menu_items.filter(function (item) {
+        return item.description.indexOf(searchTerm) !== -1;
+      });
+    });
+  };
 }
 
 })();
