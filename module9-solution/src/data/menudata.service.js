33de2ed89d3efb16d31c9861e145610785feaae3
Andrew Keen <andrewpkeen@gmail.com>
2017-11-15 04:56:58 -0500

    Created data module and MenuDataService

---
+++ module9-solution/src/data/menudata.service.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('data').service('MenuDataService', MenuDataService);
+
+MenuDataService.$inject = ['$http']
+function MenuDataService($http) {
+  var service = this;
+
+  service.getAllCategories = function () {
+    $http({
+      method: 'GET',
+      url: 'https://davids-restaurant.herokuapp.com/categories.json'
+    }).then(function (response) {
+      return response.data;
+    });
+  };
+
+  service.getItemsForCategory = function (categoryShortName) {
+    $http({
+      method: 'GET',
+      url: 'https://davids-restaurant.herokuapp.com/menu_items.json',
+      params: {
+        category: categoryShortName
+      }
+    }).then(function (response) {
+      return response.data;
+    });
+  };
+}
+
+});
