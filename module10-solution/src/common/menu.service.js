c802f7fff3dc74a8e71552b245674d04901dc58e
Andrew Keen <andrewpkeen@gmail.com>
2017-12-05 22:20:39 -0500

    Added comments and white space

--- module10-solution/src/common/menu.service.js
+++ module10-solution/src/common/menu.service.js
@@ -9,6 +9,7 @@ MenuService.$inject = ['$http', 'ApiPath', '$q'];
 function MenuService($http, ApiPath, $q) {
   var service = this;
 
+  // Regular expression for matching valid short names
   var nameMatcher = /^([A-Z]{1,2})(\d{1,2})$/;
   
   service.getCategories = function () {
@@ -31,19 +32,30 @@ function MenuService($http, ApiPath, $q) {
 
   service.getMenuItem = function (shortName) {
     shortName = shortName.toUpperCase();
+
+    // Check if the short name is valid
     var matches = nameMatcher.exec(shortName);
     if (matches) {
+
+      // Get all items in the category
       return service.getMenuItems(matches[1]).then(function (items) {
+
+        // Find the specific item we're looking for
         var item = items.menu_items.find(function (item) {
           return item.short_name === shortName;
         });
+
         if (item) {
+          // Item found, return it
           return item;
         } else {
+          // Item not found, reject the promise
           return $q.reject(shortName);
         }
       });
+      
     } else {
+      // Invalid short name, reject it
       return $q.reject(shortName);
     }
   }
